84301dbcc02632636d4f5d8bf78b5cb1e05be85d
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-03-23 13:31:18 +0800

    Time: 27 ms (97.38%), Space: 21.1 MB (90.83%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -1,45 +1,18 @@
 class Solution {
 public:
     string getSmallestString(int n, int k) {
-        //ascii 'a' decimai = 97
-        vector<int> strring(n, k / n); //均摊 之后把数字向右推
-        vector<int>::iterator ptr = strring.begin() + n - 1;
-        int remainder = k % n;
-        while (remainder > 0) {
-            if (*ptr + remainder <= 26) {
-                *ptr += remainder;
-                break;
-            }
-            else {
-                remainder = remainder + *ptr - 26;
-                *ptr = 26;
-                ptr--;
-            }
+        string res(n, 'a');
+        k -= n;
+        
+        int i = res.size() - 1;
+        while (k > 0) {
+            int tmp = min(k, 25);
+            res[i] += tmp;
+            k -= tmp;
+            --i;
         }
         
-        // two pointers
-        int i = 0, j = n - 1;
-        while (i < j) {
-            int remain = 26 - strring[j];
-            if (remain >= strring[i] - 1) {
-                strring[j] = strring[j] + strring[i] - 1;
-                strring[i] = 1;
-            }
-            else {
-                strring[i] -= remain;
-                strring[j] += remain;
-            }
-            if (strring[i] == 1)
-                i++;
-            if (strring[j] == 26)
-                j--;
-        }
-        
-        string ans = "";
-        for (auto& s : strring) {
-            ans += char(s + 96);
-        }
-        return ans;
+        return res;
         
     }
 };
