2709fe2d99bc36b92691fb1cf7bbdb9d74fb3153
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-03-30 19:48:55 +0800

    Time: 4 ms (71.72%), Space: 13.4 MB (82.28%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -11,10 +11,24 @@
  */
 class Solution {
 public:
-    int sumOfLeftLeaves(TreeNode* root, bool isLeft = false) {
-        if (!root) return 0;
-        if (!root->left && !root->right) 
-            return isLeft ? root->val : 0;
-        return sumOfLeftLeaves(root->left, true) + sumOfLeftLeaves(root->right, false);
+    void func(TreeNode* root, int* left_sum, bool left_child) {
+        if (!root) 
+            return;
+        //check if its a leaf node
+        if (root -> left == NULL && root -> right == NULL && left_child) {
+            *left_sum += root->val;
+            return;
+        }
+        
+        func(root->left, left_sum, true);
+        func(root->right, left_sum, false);
+        
+    }
+    
+    int sumOfLeftLeaves(TreeNode* root) {
+        int ans = 0;
+        func(root, &ans, false);
+        
+        return ans;
     }
 };
