26a954b0b138bc826c9a95d8af28573d7ee04afd
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-03-30 17:52:54 +0800

    Time: 0 ms (100.00%), Space: 6.1 MB (29.92%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        //optimize: recursive -> dp -> fibonacci dp
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+        
+        //inspired by: https://www.youtube.com/watch?v=RrFg9SZ8VoM
+    }
+};
