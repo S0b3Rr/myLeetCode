b7f60533f66d8970758343b8943d1f14ea20636d
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-04-03 00:04:51 +0800

    Time: 7 ms (97.50%), Space: 13.2 MB (64.59%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        vector<vector<int>> ans, group(groupSizes.size() + 1);
+        for (int i = 0; i < groupSizes.size(); i++) {
+            group[groupSizes[i]].push_back(i);
+            if (group[groupSizes[i]].size() == groupSizes[i]) {
+                ans.push_back(group[groupSizes[i]]);
+                group[groupSizes[i]].clear();
+            }
+        }
+        return ans;
+    }
+};
