35000e79e618f88eb034236cf76078ca0cc5de56
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-04-08 15:05:32 +0800

    Time: 36 ms (84.52%), Space: 12.3 MB (57.68%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void islandRipper(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || j < 0 || i == grid.size() || j == grid[0].size() || grid[i][j] == '0')
+            return;
+        grid[i][j] = '0';
+        islandRipper(grid, i + 1, j);
+        islandRipper(grid, i - 1, j);
+        islandRipper(grid, i, j + 1);
+        islandRipper(grid, i, j - 1);
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1') {
+                    islandRipper(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
