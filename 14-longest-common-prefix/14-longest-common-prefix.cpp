81e4565d3e7303ce9fbf321bbb141e3a545b5eb5
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-09-07 17:13:17 +0800

    Time: 6 ms (62.93%), Space: 9.1 MB (95.41%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        //string ans = "";
+        int str_idx = 0;
+        int minLength = 500;
+        for (int j = 0; j < strs.size(); j++)
+            minLength = std::min(minLength, int(strs[j].size()));
+        int ptr = 0;
+        while (ptr < minLength) {
+            char sample = strs[0][ptr];
+            for (int i = 1; i < strs.size(); i++) {
+                if (strs[i][ptr] != sample)
+                    goto End;
+            }
+            ptr++;
+        }
+        End:
+        return strs[0].substr(0, ptr);
+    }
+};
