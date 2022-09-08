45e39d1dc5ced6e56f38a07adf2c7f8f50c9f3dd
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-09-08 14:20:45 +0800

    Time: 20 ms (49.42%), Space: 21.9 MB (15.69%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -29,3 +29,9 @@ public:
         inorderConvert(node -> right, inorderTree);
     }
 };
+
+static const auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
+}();
