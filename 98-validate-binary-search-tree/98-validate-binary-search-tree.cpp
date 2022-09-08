5bbb30b45893480e6b05496b9fb856d98fb0eba6
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-09-08 14:23:37 +0800

    Time: 12 ms (86.76%), Space: 21.7 MB (30.77%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,24 +11,41 @@
  */
 class Solution {
 public:
-    bool isValidBST(TreeNode* node) {
-        vector<int> inorderTree;
-        inorderConvert(node, inorderTree);
-        for (int i = 0; i < inorderTree.size() - 1; i++) {
-            if (inorderTree[i] >= inorderTree[i + 1]) 
-                return false;
-        }
-        return true;
-    }
+//     bool isValidBST(TreeNode* node) {
+//         vector<int> inorderTree;
+//         inorderConvert(node, inorderTree);
+//         for (int i = 0; i < inorderTree.size() - 1; i++) {
+//             if (inorderTree[i] >= inorderTree[i + 1]) 
+//                 return false;
+//         }
+//         return true;
+//     }
     
-    void inorderConvert (TreeNode* node, vector<int>& inorderTree) {
-        if (!node)
-            return;
-        inorderConvert(node -> left, inorderTree);
-        inorderTree.push_back(node -> val);
-        inorderConvert(node -> right, inorderTree);
+//     void inorderConvert (TreeNode* node, vector<int>& inorderTree) {
+//         if (!node)
+//             return;
+//         inorderConvert(node -> left, inorderTree);
+//         inorderTree.push_back(node -> val);
+//         inorderConvert(node -> right, inorderTree);
+//     }
+    
+    
+//-----------------------------------solution 2----------------------------------------------
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, NULL, NULL);
     }
+
+    bool isValidBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+        if(!root) 
+            return true;
+        if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val)
+            return false;
+        return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
+}
+//-------------------------------------------------------------------------------------------
 };
+
+
 
 static const auto speedup = []() {
     std::ios::sync_with_stdio(false);
