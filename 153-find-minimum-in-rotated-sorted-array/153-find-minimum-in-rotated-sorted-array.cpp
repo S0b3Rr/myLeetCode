9a7872d98d893bf4907ee720bb92f5b66c712e40
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-09-24 01:06:31 +0800

    Time: 0 ms (100.00%), Space: 10.4 MB (22.48%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -25,3 +25,10 @@ public:
         return currMin = currMin < nums[index] ? currMin : nums[index];
     }
 };
+
+static const auto speedup = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
+}();
