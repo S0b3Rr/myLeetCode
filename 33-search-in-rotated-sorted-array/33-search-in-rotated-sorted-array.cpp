3e98eddaefef77755841dac2e2e3a015280f5115
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-09-24 01:05:03 +0800

    Time: 11 ms (24.04%), Space: 11.2 MB (29.23%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -38,3 +38,10 @@ public:
             return findIndex(0, pivot - 1, nums, target);
     }
 };
+
+static const auto speedup = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
+}();
