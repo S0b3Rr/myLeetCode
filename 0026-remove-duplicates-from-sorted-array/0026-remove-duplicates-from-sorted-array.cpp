08fe5a79cf4714b23e2723624c8e4509a82f4503
S0b3Rr <94901095+S0b3Rr@users.noreply.github.com>
2022-11-29 11:56:15 +0800

    Time: 15 ms (83.99%), Space: 18.5 MB (37.94%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,8 +3,8 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int i = 0;
         for (int n : nums)
-        if (!i || n > nums[i-1])
-            nums[i++] = n;
+            if (!i || n > nums[i-1])
+                nums[i++] = n;
         return i;
     }
 };
